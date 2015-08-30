171dc73a04341c3cd30890bd2c0abdf7f3cfa177
AKI Shunichiro <register.develop@nana4.net>
2015-08-30 14:35:24 +0900

    メイン画面、設定画面を更新

--- app/src/main/java/net/nana4/contactlenstimer/MainActivity.java
+++ app/src/main/java/net/nana4/contactlenstimer/MainActivity.java
@@ -1,17 +1,86 @@
 package net.nana4.contactlenstimer;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private DateFormat saveDateFormat;
+    private DateFormat viewDateFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 日付フォーマット
+        saveDateFormat = new SimpleDateFormat(getString(R.string.save_date_format));
+        viewDateFormat = android.text.format.DateFormat.getLongDateFormat(getApplicationContext());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        boolean lensSeparately = prefs.getBoolean("lends_separately", false);
+
+        try {
+            // 右目の保存設定を表示
+            Calendar rightEyeCalendar = Calendar.getInstance();
+            String rightEyeDate = prefs.getString("right_use_start_date", null);
+            if (rightEyeDate != null) {
+                rightEyeCalendar.setTime(saveDateFormat.parse(rightEyeDate));
+                setTextUseDate(R.id.textViewRightUseStartDate, rightEyeCalendar);
+            }
+
+            // 左目の保存設定を表示
+            if (lensSeparately) {
+                Calendar leftEyeCalendar = Calendar.getInstance();
+                String leftEyeDate = prefs.getString("left_use_start_date", null);
+                if (leftEyeDate != null) {
+                    leftEyeCalendar.setTime(saveDateFormat.parse(leftEyeDate));
+                    setTextUseDate(R.id.textViewLeftUseStartDate, leftEyeCalendar);
+                }
+            }
+        } catch (ParseException e) {
+            Log.e(TAG, "保存されている日付が不正", e);
+        }
+
+        TextView textViewRightEye = (TextView) findViewById(R.id.textViewRightEye);
+        RelativeLayout layoutLeftEye = (RelativeLayout) findViewById(R.id.layoutLeftEye);
+
+        // 左右別々の状態に応じて画面構成を変更
+        if (lensSeparately) {
+            // 右目の表示を 右目 に切り替え
+            textViewRightEye.setText(R.string.right_eye);
+            // 左目のレイアウトを表示
+            layoutLeftEye.setVisibility(View.VISIBLE);
+        } else {
+            // 右目の表示を 右目/左目 に切り替え
+            textViewRightEye.setText(R.string.both_eyes);
+            // 左目のレイアウトを非表示
+            layoutLeftEye.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -19,6 +88,89 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void onClickUseStartDate(final View v) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        // Preferenceに保存するキーを求める
+        final String saveKey;
+        if (v.getId() == R.id.textViewRightUseStartDate) {
+            saveKey = "right_use_start_date";
+        } else if (v.getId() == R.id.textViewLeftUseStartDate) {
+            saveKey = "left_use_start_date";
+        } else {
+            throw new IllegalStateException();
+        }
+
+        // 現在の時刻
+        Calendar nowCalendar = Calendar.getInstance();
+        int year = nowCalendar.get(Calendar.YEAR);
+        int monthOfYear = nowCalendar.get(Calendar.MONTH);
+        int dayOfMonth = nowCalendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view,
+                                  int year, int monthOfYear, int dayOfMonth) {
+                Calendar selectCalendar = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+
+                // 開始日を保存
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(saveKey, saveDateFormat.format(selectCalendar.getTime()));
+                editor.commit();
+
+                // 画面に反映
+                setTextUseDate(v.getId(), selectCalendar);
+            }
+        }, year, monthOfYear, dayOfMonth);
+
+        datePicker.show();
+    }
+
+    /**
+     * 開始日と終了日をTextViewに設定します
+     *
+     * @param startViewId
+     * @param baseCalendar
+     */
+    private void setTextUseDate(int startViewId, Calendar baseCalendar) {
+        // 開始日を設定
+        TextView startDateView = (TextView) findViewById(startViewId);
+        startDateView.setText(viewDateFormat.format(baseCalendar.getTime()));
+
+        // 開始日/終了日の対応
+        int endViewId;
+        if (startViewId == R.id.textViewRightUseStartDate) {
+            endViewId = R.id.textViewRightUseEndDate;
+        } else if (startViewId == R.id.textViewLeftUseStartDate) {
+            endViewId = R.id.textViewLeftUseEndDate;
+        } else {
+            throw new IllegalStateException();
+        }
+
+        // 終了日を設定
+        TextView endDateView = (TextView) findViewById(endViewId);
+        addUseDate(baseCalendar);
+        endDateView.setText(viewDateFormat.format(baseCalendar.getTime()));
+    }
+
+    /**
+     * コンタクトレンズの利用日数を加算します
+     *
+     * @param baseCalendar
+     */
+    private void addUseDate(Calendar baseCalendar) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        String lendsType = prefs.getString("lends_type", "2w");
+
+        switch (lendsType) {
+            case "2w":
+                baseCalendar.add(Calendar.DAY_OF_MONTH, 14);
+                break;
+            case "1m":
+                baseCalendar.add(Calendar.MONTH, 1);
+        }
     }
 
     @Override
--- app/src/main/java/net/nana4/contactlenstimer/SettingsActivityFragment.java
+++ app/src/main/java/net/nana4/contactlenstimer/SettingsActivityFragment.java
@@ -1,15 +1,91 @@
 package net.nana4.contactlenstimer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import org.bostonandroid.timepreference.TimePreference;
+
 public class SettingsActivityFragment extends PreferenceFragment {
+    private ListPreference lendsType;
+    private SwitchPreference lendsSeparately;
+    private SwitchPreference notification;
+    private TimePreference notificationTime;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_settings);
+
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        lendsType = (ListPreference) findPreference("lends_type");
+        lendsSeparately = (SwitchPreference) findPreference("lends_separately");
+        notification = (SwitchPreference) findPreference("notification");
+        notificationTime = (TimePreference) findPreference("notification_time");
+
+        // レンズ種類変更時
+        lendsType.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object value) {
+                if (value == null) {
+                    return false;
+                }
+
+                // レンズ種類をsummaryに表示
+                int listIndex = lendsType.findIndexOfValue((String) value);
+                preference.setSummary(lendsType.getEntries()[listIndex]);
+                return true;
+            }
+        });
+
+        // 左右別々変更時
+        lendsSeparately.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object value) {
+                if ((Boolean) value) {
+                    // 左右別々に設定した場合、右目の日付を左目にコピーする。
+                    String rightUseStartDate = prefs.getString("right_use_start_date", null);
+
+                    if (rightUseStartDate != null) {
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("left_use_start_date", rightUseStartDate);
+                        editor.commit();
+                    }
+                } else {
+                    // 左右別々に設定しない場合、左目の日付を削除する
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.remove("left_use_start_date");
+                    editor.commit();
+                }
+
+                return true;
+            }
+        });
+
+        // 通知設定変更時
+        notification.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object value) {
+                // 通知設定が無効な場合、通知時間の選択を無効にする
+                notificationTime.setEnabled((Boolean) value);
+
+                return true;
+            }
+        });
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // レンズ種類をsummaryに表示
+        int listIndex = lendsType.findIndexOfValue(lendsType.getValue());
+        lendsType.setSummary(lendsType.getEntries()[listIndex]);
+
+        // 通知設定が無効な場合、通知時間の選択を無効にする
+        notificationTime.setEnabled(notification.isChecked());
+    }
+
 }
